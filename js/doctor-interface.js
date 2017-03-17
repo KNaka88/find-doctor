ae09cf42ae20545cacdccda16fc3f44f6eec6e74
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-03-17 15:15:11 -0700

    add css, add result display when rno doctors found

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,42 +4,44 @@ var googleApiKey = require('./../.env').googleApiKey;
 
 var displayResults = function(resultArray){
 
-  for(var i=0; i<resultArray.length; i++){
-    $("#result").append(
-      "<div class='row'>" +
-        "<div class='col-md-12'>" +
-          "<h4>" + resultArray[i].name +"</h4>"+
+  if(resultArray[0] === undefined){
+    $("#result").append("<h2>No Result</h2>");
+  }else{
+    for(var i=0; i<resultArray.length; i++){
+      $("#result").append(
+        "<div class='show_doctor'>" +
+        "<div class='row'>" +
+        "<div class='col-md-6 image'>" +
+        "<h2>" + resultArray[i].name +"</h2>"+
+        "<img src='"+ resultArray[i].image_url + "' alt='"+ resultArray[i].name +"'/>" +
+        "<p>My Specialities: <span id='specialities'>"+ resultArray[i].specialities +"</span></p>" +
+        "<p>I Speak: <span id='languages'>" +resultArray[i].languages + "</span></p>" +
         "</div>" +
-        "<div class='col-md-4'>" +
-          "<img src='"+ resultArray[i].image_url + "' alt='"+ resultArray[i].name +"'/>" +
-          "<p>My Specialities: <span id='specialities'>"+ resultArray[i].specialities +"</span></p>" +
-          "<p>I Speak: <span id='languages'>" +resultArray[i].languages + "</span></p>" +
+        "<div class='col-md-6 contact'>" +
+        "<h3>Contact Info</h3>" +
+        "<p>Phone Number: <span id='phone_number'>"+ resultArray[i].phone_number +"</span></p>" +
+        "<p>Website: <a id='website' href='"+resultArray[i].website+"'>"+ resultArray[i].website+"</a></p>" +
+        "<p>Address:<br></p>" +
+        "<p>" +
+        "<span id='street'>"+resultArray[i].street+"</span><span id='street2'>"+ resultArray[i].street2+"</span>,&nbsp;<span id='state'>"+resultArray[i].state+"</span>,&nbsp;<span id='zip'>"+resultArray[i].zip+"</span>" +
+        "</p>" +
+        "<button type='button class='location_button'>See Location</button>" +
         "</div>" +
-        "<div class='col-md-8'>" +
-          "<h3>Where I practice</h3>" +
-          // "<div id='map'></div>" +
+        "<div class='col-md-12 summary'>" +
+        "<h3>Summary</h3>" +
+        "<p id='description'>"+resultArray[i].description+"</p>" +
         "</div>" +
-        "<div class='col-md-4'>" +
-          "<h3>Contact Info</h3>" +
-          "<p>Phone Number: <span id='phone_number'>"+ resultArray[i].phone_number +"</span></p>" +
-          "<p>Website: <a id='website' href='"+resultArray[i].website+"'>"+ resultArray[i].website+"</a></p>" +
-          "<p>Address:<br></p>" +
-          "<p>" +
-            "<span id='street'>"+resultArray[i].street+"</span><span id='street2'>"+ resultArray[i].street2+"</span>,&nbsp;<span id='state'>"+resultArray[i].state+"</span>,&nbsp;<span id='zip'>"+resultArray[i].zip+"</span>" +
-          "</p>" +
+        "<div class='col-md-12 bio'>" +
+        "<h3>Bio</h3>" +
+        "<p id='bio'>"+resultArray[i].bio+"</p>" +
         "</div>" +
-        "<div class='col-md-8'>" +
-          "<h3>Summary</h3>" +
-          "<p id='description'>"+resultArray[i].description+"</p>" +
-        "</div>" +
-        "<div class='col-md-12'>" +
-          "<h3>Bio</h3>" +
-          "<p id='bio'>"+resultArray[i].bio+"</p>" +
-        "</div>" +
-      "</div> <!--end of row -->"
-    );
+        "</div> <!--end of row -->" +
+        "<p class='lat'>"+resultArray[i].lat+"</p>" +
+        "<p class='lon'>"+resultArray[i].lon+"</p>" +
+        "</div>"
+      );
+    }
   }
-
 };
 
 
@@ -50,6 +52,5 @@ $(function(){
     var query = $("#query").val();
     var newDoctor = new Doctor();
     newDoctor.getDoctors(query, displayResults);
-    // newDoctor.getDoctors(query, displayResults);
   });
 });
--- js/location-interface.js
+++ js/location-interface.js
@@ -1,33 +1,56 @@
-function initMap() {
+function initMap(locaton) {
     var map = new google.maps.Map(document.getElementById('map'), {
         //Epicodus Location
         center: {lat:45.5206223, lng:-122.6795871 },
         zoom: 15
     });
+
     var infoWindow = new google.maps.InfoWindow({map: map});
 
     map.addListener('click', function(e) {
         placeMarkerAndPanTo(e.latLng, map);
     });
 
-    //    Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here.');
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+    var locationButtonElements = document.getElementsByClassName("location_button");
+    console.log(locationButtonElements);
+
+    //
+    // document.getElementById("get").addEventListener('click', function(event) {
+    //
+    //     // var latArray = document.getElementsByClassName("lat");
+    //     // var lonArray = document.getElementsByClassName("lon");
+    //     var lat = $(this).text();
+    //     var lon = $(this).text();
+    //     console.log("clicked");
+    //     console.log(lat);
+    //     console.log(lon)
+    //
+    //     for(var i=0; i<latArray.length; i++){
+    //       console.log("check");
+    //       var lat = latArray[i].val;
+    //       var lon = lonArray[i].val;
+    //       console.log(lat);
+    //       console.log(lon);
+
+          var uluru = {lat: -25.363, lng: 131.044};
+
+
+
+          var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+          });
+
+      //
+      // })
+
+      // }
+
+
+
+    //
+    // });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
